732160a74de1b5f5b73285bb21c7a7a09b63fa40
zt8media <zt8media@gmail.com>
2024-07-25 10:46:20 -0400

    key club

--- src/portfolio.js
+++ src/portfolio.js
@@ -130,7 +130,7 @@ const educationInfo = {
       subHeader: "Associate in Arts",
       duration: "Augus 2013 - May 2017",
       desc: "Went to an early college, so graduated with my associate's and high school diploma at the same time.",
-      descBullets: [
+      descBullets: ["Partipated in Key Club"
        
       ]
     },
